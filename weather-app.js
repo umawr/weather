99402e4a8525bc2681156e4e1043775b2c8aa787
Umair <umaircapalot@gmail.com>
2026-07-05 19:01:24 +0530

    Update weather-app.js

--- weather-app.js
+++ weather-app.js
@@ -1,28 +1,79 @@
 'use strict';
-const apiKey = 'b0380e75c8ba98bfd79e25ab3e08fa66';
-// const apiUrl =
-//   'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=hyderabad';
 
-const searchBtn = document.querySelector('.search');
-const locationInput = document.querySelector('.location');
+const weatherMap = {
+  Clouds: 'cloud',
+  Clear: 'clear_day',
+  Rain: 'rainy',
+  Drizzle: 'rainy',
+  Thunderstorm: 'thunderstorm',
+  Snow: 'ac_unit',
+  Mist: 'foggy',
+  Haze: 'foggy',
+  Fog: 'foggy',
+};
+
+let locationInput = document.querySelector('.location');
 
 async function checkWeather(city) {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?&units=metric&q=${city}&appid=${apiKey}`);
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?&units=metric&q=${city}&appid=b0380e75c8ba98bfd79e25ab3e08fa66`,
+  );
   const data = await response.json();
+  const locationWeather = data;
+
+  const condition = data.weather[0].main;
+  const iconName = weatherMap[condition] || 'help';
 
   console.log(data);
+  const precipitation = data.rain?.['1h'] ?? data.snow?.['1h'] ?? '0';
+  document.querySelector('.precipitation').innerHTML =
+    `${precipitation} Precipitation`;
   document.querySelector('.location').innerHTML = data.name;
   document.querySelector('.temperature').innerHTML =
-    `<span class="material-symbols-outlined sunnyIcon">sunny</span> ${data.main.temp}&deg;C`;
-  document.querySelector('.humidity').innerHTML = data.main.humidity;
-  document.querySelector('.wind').innerHTML = data.wind.speed;
+    `<span class="material-symbols-outlined sunnyIcon">${iconName}</span> ${data.main.temp}&deg;C`;
+  document.querySelector('.humidity').innerHTML =
+    `${data.main.humidity}% Humidity`;
+  document.querySelector('.wind').innerHTML = `${data.wind.speed} Km/h Wind`;
 }
 
-searchBtn.addEventListener('click', () => {
-  const cityName = locationInput.value;
-  if(cityName){
-  checkWeather(cityName);
-  } else {
-    alert('Please enter name')
+async function fetchCityCoordinates(query) {
+  const url = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(query)}`;
+
+  const options = {
+    headers: {
+      'User-Agent': 'MyWeatherApp_ForLearning_ContactEmail@example.com',
+    },
+  };
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    if (data && data.length > 0) {
+      console.table(data[0]);
+      document.querySelector('.coordinates').innerHTML =
+        `${data[0].lat}, ${data[0].lon}`;
+
+          const stateCountry = data[0].display_name;
+          console.log(stateCountry);
+      document.querySelector('.stateCountry').innerHTML = `${stateCountry}`
+
+    } else {
+      console.log('No results found for', query);
+    }
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+
+
+}
+locationInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    fetchCityCoordinates(locationInput.value);
+    const cityName = locationInput.value;
+    if (cityName) {
+      checkWeather(cityName);
+    } else {
+      alert('Please enter a city name!');
+    }
   }
 });
